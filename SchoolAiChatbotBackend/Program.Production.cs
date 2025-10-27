cec7d09418d71db91871fa18f8eb6f9078ce6ad8
Sadashiv Byakod <sadabyakod@gmail.com>
2025-10-27 18:06:44 +0530

    Update Program.Production.cs and add integration test file

--- SchoolAiChatbotBackend/Program.Production.cs
+++ SchoolAiChatbotBackend/Program.Production.cs
@@ -8,6 +8,9 @@ using System.Text.Json;
 // using SchoolAiChatbotBackend.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 // Update CORS policy to allow any origin
 builder.Services.AddCors(options =>
 {
